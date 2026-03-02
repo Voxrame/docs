55d6e70f5cbb628b25d62dec2a978b446363522f
Alexander Chibrikin <alek13.me@gmail.com>
2026-03-02 20:00:37 +0300

    Stylize mini-toc; change fonts.

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,6 +1,6 @@
 import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
-import './custom.css'
+import './custom.pcss'
 
 import { NolebaseHighlightTargetedHeading } from '@nolebase/vitepress-plugin-highlight-targeted-heading/client'
 import '@nolebase/vitepress-plugin-highlight-targeted-heading/client/style.css'
